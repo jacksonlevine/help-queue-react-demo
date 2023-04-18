be8fddf90f73739a32d60efc82cb9e33fb4c5963
Jackson Levine <jack@netbug.us>
2023-04-18 09:00:57 -0700

    create demo help queue

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ticketsImage from "./../img/ticket.jpg";
+
+function Header(){
+    return (
+        <React.Fragment>
+            <h1>Help Queue</h1>
+            <img src={ticketsImage} alt="tickets" />
+        </React.Fragment>
+    );
+}
+
+export default Header;
